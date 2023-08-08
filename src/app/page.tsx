9ff43d35f04ed99ef902fa40c278dd70a5e8e4b6
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-08 01:56:24 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 import Image from 'next/image'
+import Header from './header'
+import AboutSection from './about'
 export default function Home() {
   return (
-    <main className="">
-      <div className="introduction-container">
-        <div className="text-container">
-          <h1>
-            <div className="basic-text">Hi, I'm</div>
-            <div className="name-text">Hunter Weisner</div>
-            <div className="basic-text">A Software Developer</div>
-          </h1>
-        </div>
-        <div className="headshot-container">
-          <img src="/hunter-weisner-headshot-cold.png" alt="Photo of Hunter Weisner" />
-        </div>
-      </div>
+    <main className="bg-gray-800 h-full w-full">
+      {Header()}
+      {AboutSection()}
     </main>
   )
 }
