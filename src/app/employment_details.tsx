3a73fa451e6d59106a595166a53c692986c5d9e4
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-08 01:57:24 -0400

    Create employment_details.tsx
    created format for employment descriptions

---
+++ src/app/employment_details.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function Employment_details(job_title: string, date: string, employere: string, location: string, responsibilites: string){
+    return(
+        <div>
+            <p className='text-white mt-2'>{job_title}</p>
+            <button className="bg-teal-400 text-gray-800 rounded-2xl font-extrabold  text-center w-fit pl-3 pr-3 mt-2">Show Employment Details</button>
+            <div>
+                <p className='text-white mt-2'>{date}</p>
+                <p className='text-white mt-2'>{employere}</p>
+                <p className='text-white mt-2'>{location}</p>
+                <p className='text-white mt-2'>{responsibilites}</p>
+                <button className="bg-teal-400 text-gray-800 rounded-2xl font-extrabold  text-center w-fit pl-3 pr-3 mt-2">Hide Details</button>
+            </div>
+        </div>
+    )
+}
