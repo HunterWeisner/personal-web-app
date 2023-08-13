9d5d8e2f84c65a6fe201fabd0b95792f47e3e184
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-13 00:43:53 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
 import Header from './header'
 import AboutSection from './about'
+import More_information from './more_information'
 export default function Home() {
   return (
     <main className="bg-gray-800 h-full w-full">
       {Header()}
       {AboutSection()}
+      {More_information()}
     </main>
   )
 }
