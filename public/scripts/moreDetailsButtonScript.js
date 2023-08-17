d6ba36336f32036ab11c101261e8851386a623b2
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-17 12:26:53 -0400

    Update moreDetailsButtonScript.js
    button expand/collapse scripts

--- public/scripts/moreDetailsButtonScript.js
+++ public/scripts/moreDetailsButtonScript.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+console.log("script loaded buttons")
+
+const buttonFunctions = {
+    ExpandOnClick: (e)=>{
+        console.log("button clicked");
+        let details_div = e.target.parentNode.getElementsByClassName("more-details-div")[0];
+        if(details_div.classList.contains("hidden")){
+            details_div.classList.remove("hidden");
+        }else{
+            details_div.classList.add("hidden");
+        }},      
+    CollapseOnClick: (e)=>{
+        let details_div = e.target.parentNode;
+        details_div.classList.add("hidden")}
+}
+
+export default buttonFunctions;
+
+
