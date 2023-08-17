14b3b047ea0fb94bce4060ef35a91dd10c216898
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-17 12:17:42 -0400

    Update layout.tsx
    removing uneeded script load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,11 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          src = '../scripts/moreDetailsButtonScript.js'
-        />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
