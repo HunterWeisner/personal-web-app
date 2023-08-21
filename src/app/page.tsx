367666874766822609c4c2d32275d96030b11ca9
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-20 20:37:24 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AboutSection from '../../public/page_sections/home_about'
 import More_information from '../../public/page_sections/more_information'
 export default function Home() {
   return (
-    <main className="bg-gray-800 h-full w-full">
+    <main className="bg-gray-900 ml-auto mr-auto">
       {Header()}
       {AboutSection()}
       {More_information()}
