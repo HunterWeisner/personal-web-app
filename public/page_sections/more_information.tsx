99ee2dd1fb4a05287979494e1da951494007e118
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-20 20:37:17 -0400

    Update more_information.tsx

--- public/page_sections/more_information.tsx
+++ public/page_sections/more_information.tsx
@@ -1,13 +1,28 @@
 'use client';
 import Job_experience from "../components/job_experience";
 import Education from "../components/education";
-
+import Flex_Container from "../components/flex_containters";
+import { Flex_Container_item } from "../interfaces/flex_container_item_interface";
+import Job from "../components/job";
 
 export default function More_information(){
+    const children: Flex_Container_item[] = [
+      {
+        id: "education",
+        child: <Education/>
+      },
+      {
+        id: "job_experience",
+        child: <Job_experience/>
+      }
+    ];
     return(
-    <div>
-      <Education/>
-      <Job_experience/>
-    </div>
+      <div>
+        
+          <Flex_Container
+            children={children}
+          />
+    
+      </div>
     )
 }
