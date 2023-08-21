8d8ede75895dd0f78681ae9c49dae3669cfadd10
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-20 20:36:53 -0400

    Update job_experience.tsx

--- public/components/job_experience.tsx
+++ public/components/job_experience.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import DropDownButton from "./dropDownButton";
 import buttonFunctions from "../scripts/moreDetailsButtonScript";
-import {Job} from "../interfaces/job";
+import {Job_interface} from "../interfaces/job_interface";
+import Job from "./job";
 import employment_history  from '../data/employment_history.json' assert {type: "json"};
 
 interface Props{
@@ -9,52 +10,31 @@ interface Props{
 
 const Job_experience: React.FC<Props>=({
 })=>{
-    const jobs = employment_history as Job[];
-    var experience: Array<JSX.Element> = []; 
     let key: number = 0;
     var keyGenerator = ()=>{
-        return key++;
+        return 'job_experience' + String(key++);
     }
-
-    for(var i: number = 0; i < jobs.length; i++){
-        experience.push(
-            <div className="flex gap-1 mt-2">
-                <p className='text-white'>{jobs[i].job_title}</p>
-                <DropDownButton
-                    border="none"
-                    color=""
-                    height="1.3rem"
-                    onClick={buttonFunctions.ExpandOnClick}
-                    radius=""
-                    width="1.3rem"
-                    children = {<svg id="triangle" viewBox="0 0 100 100">
-                        <polygon points="50 15, 100 100, 0 100"/></svg>}
-                />
-                <div className="more-details-div hidden" id={jobs[i].job_title.replace(/\s/g,'-')
-                    + "-details"} aria-expanded="false">
-                    <p className='text-white mt-2'>{jobs[i].date}</p>
-                    <p className='text-white mt-2'>{jobs[i].employere}</p>
-                    <p className='text-white mt-2'>{jobs[i].location}</p>
-                    <p className='text-white mt-2'>{jobs[i].responsibilities}</p>
-                    <DropDownButton
-                        border="none"
-                        color="white"
-                        height="auto"
-                        onClick={buttonFunctions.CollapseOnClick}
-                        radius="100%"
-                        width="200px"
-                        children = "collapse details"
-                    />
-                </div>
-            </div>
-        );
-    } 
+    const jobs = employment_history as Job_interface[];
+    const experience: Array<JSX.Element> = jobs.map(job =>
+        <div className="mt-4">   
+            <p className='text-xl 
+                font-semibold text-teal-500 md:text-2xl lg:text-4xl'>{job.job_title}</p>
+            <Job
+                date={job.date}
+                employere={job.employere}
+                location={job.location}
+                responsibilities={job.responsibilities}
+            />
+        </div>
+    ); 
     return(
-        <div className="content-center m-4">
-            <h3 className='text-white text-2xl'>Experience</h3>
-            <ul>{experience.map(item =>
-                <li key={keyGenerator()}>{item}</li>)}
-            </ul>
+        <div key={'job_experience-div'} className="gap-1 m-5">
+            <div className="content-center ">
+                <h3 className='text-white text-3xl font-semibold md:text-4xl'>Work Experience</h3>
+                <ul className="first-of-type:-mt-4">{experience.map(item =>
+                    <li key={keyGenerator()}>{item}</li>)}
+                </ul>
+            </div>
       </div>
     )
 }
