eb1391f61acd59fcb0208977a6dd68751aecd77e
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-20 20:36:45 -0400

    Update education.tsx

--- public/components/education.tsx
+++ public/components/education.tsx
@@ -1,31 +1,38 @@
 import React from "react";
 import Bachelor from "./bachelor";
 import bachelors  from '../data/bachelors.json' assert {type: "json"};
-import { bachelor } from "../interfaces/bachelor";
-import exp from "constants";
+import { Bachelor_interface } from "../interfaces/bachelor_interface";
 
 interface Props{}
 
 const Education: React.FC<Props> = ({
 
 })=>{
-    const bacholers = bachelors as bachelor[];
-    let bachelors_jsx: Array<JSX.Element> = [];
+    const bacholers = bachelors as Bachelor_interface[];
+   
     let key: number = 0;
-    let keyGenerator = ()=>{return key++;}
-    for(var i: number = 0; i < bachelors.length; i++){
-        bachelors_jsx.push(<Bachelor
-                            degree_title={bachelors[i].degree_title}
-                            major={bachelors[i].Major}
-                            university={bachelors[i].University}/>)
-    }
+    let keyGenerator = ()=>{return 'education'+ String(key++);}
+    const bachelors_jsx: Array<JSX.Element> = bachelors.map(bachelor=>
+        <Bachelor
+            degree_title={bachelor.degree_title}
+            major={bachelor.Major}
+            university={bachelor.University}
+            date={bachelor.date}/>
+    );
     return(
-        <div className="content-center m-4">
-            <h3 className='text-white text-2xl'>Education</h3>
-            <ul>
-                {bachelors_jsx.map(item =><li key={keyGenerator()}>{item}</li>)}
-            </ul>
-        </div>
+        <section>
+
+            <div key={'education-div'} className="content-center 
+                    m-6 bg-gray-900 rounded-sm">
+                <h3 className='text-white text-3xl font-bold lg:text-6xl 
+                    md:text-4xl'>Education</h3>
+                <ul>
+                    {bachelors_jsx.map(item =>
+                        <li key={keyGenerator()}>{item}</li>)}
+                </ul>
+            </div>
+                    
+        </section>
     )
 }
 
