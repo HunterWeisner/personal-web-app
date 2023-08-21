651bd08ea4358d495889cf0c0cef130f3c885d99
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-20 20:36:49 -0400

    Update header.tsx

--- public/components/header.tsx
+++ public/components/header.tsx
@@ -17,21 +17,27 @@ const Header: React.FC<Props> = ({
     bottomText
 })=>{
     return(
-        <div className="content-center flex">
-        <div className="w-2/3 inline-block m-auto">
-            <h1 className='mr-3 ml-5'>
-            <div className="text-white text-xl mt-2"><p>{topText}</p></div>
-            <div className="bg-teal-400 text-gray-800 rounded-2xl font-extrabold text-2xl text-center w-fit pl-3 pr-3 -ml-3 -mr-3 mt-2">{centerText}</div>
-            <div className="text-white text-xl mt-2"><p>{bottomText}</p></div>
-            </h1>
-        </div>
-        <div className="w-1/3 inline-block center m-3">
-            <Image
-              src = {image}
-              alt = {altText}
-            />
-        </div>
-    </div>
+        <div className="m-auto mb-12 max-w-5xl">
+            <div className="float-none">
+                <Image
+                src = {image}
+                alt = {altText}
+                className="rounded-full border-teal-400 border-solid border-2
+                    m-auto max-width headshot self-center mt-4"
+                priority
+                />
+            </div>
+            <div className="m-auto text-center mt-4">
+                <h1 className='m-auto'>
+                    <div className="text-white text-3xl mt-2 lg:text-7xl md:text-5xl"><p>{topText}</p></div>
+                    <div className="bg-teal-400 text-gray-900 rounded-2xl font-extrabold 
+                        text-4xl text-center m-auto w-fit pl-3 pr-3 mt-2 lg:pl-6 lg:pr-6 lg:text-8xl lg:rounded-3xl
+                        md:text-6xl">{centerText}</div>
+                    <div className="text-white text-3xl mt-2 lg:text-7xl md:text-5xl"><p>{bottomText}</p></div>
+                </h1>
+            </div>
+            
+     </div>
     );
 }
 
