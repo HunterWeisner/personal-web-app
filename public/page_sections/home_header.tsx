785474aab744493eea509a49db83672a151c7a97
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-20 20:37:16 -0400

    Update home_header.tsx

--- public/page_sections/home_header.tsx
+++ public/page_sections/home_header.tsx
@@ -1,11 +1,11 @@
-import headShot from "public/images/hunter-weisner-headshot-cold.png"
+import headShot from "../../public/images/headshot-with-bg-cropped.png"
 import Header from "../components/header"
 export default function Home_page_header() {
   return (
     <Header
       image={headShot}
       altText="picture of Hunter Weisner"
-      topText="Hi I'm"
+      topText="Hi, I'm"
       centerText="Hunter Weisner"
       bottomText="A Web Developer"
     />
