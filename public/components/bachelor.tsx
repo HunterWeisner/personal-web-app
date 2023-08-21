0415256edc0308fa1990481f3de56ffd59b92f22
HunterWeisner <112354824+HunterWeisner@users.noreply.github.com>
2023-08-20 20:36:41 -0400

    Update bachelor.tsx

--- public/components/bachelor.tsx
+++ public/components/bachelor.tsx
@@ -1,22 +1,32 @@
-import exp from "constants";
 import React from "react";
 
 interface Props{
     degree_title: string;
     major: string;
     university: string;
+    date: string;
 }
 
 const Bachelor: React.FC<Props>=({
     degree_title,
     major,
-    university
+    university,
+    date
 })=>{
     return(
         <div>
-            <p className='text-white mt-2'>{degree_title}</p>
-            <p className='text-white mt-2'>{major}</p>
-            <p className='text-white mt-2'>{university}</p>
+            <h3 className="text-xl text-teal-500 font-bold md:text-2xl lg:text-4xl">{university}</h3>
+            <p className="text-md italic text-teal-400 md:text-xl lg:text-3xl">{date}</p>
+            <div className="mt-3">
+            <h4 className="text-white text-xl font-semibold md:text-2xl lg:text-4xl">Degree</h4>
+            <p className='text-white md:text-lg lg:text-2xl'>
+                {degree_title}</p>
+            </div>
+            <div className="mt-3 items-center">
+            <h4 className="text-white text-xl font-semibold md:text-2xl lg:text-4xl">Major</h4>
+            <p className='text-white md:text-lg lg:text-2xl'>
+                 {major}</p>
+            </div>
         </div>
     )
 }
